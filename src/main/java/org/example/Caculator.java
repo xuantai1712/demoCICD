db4a0b23b8dc26ba0a8364b12732f6f998acf544
tai <taibvps37458@fpt.edu.vn>
2024-11-25 17:08:25 +0700

    update fail

--- src/main/java/org/example/Caculator.java
+++ src/main/java/org/example/Caculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class Caculator implements Icaculator{
 
     public Integer sum(Integer a, Integer b){
-        return 0;
+        return a/b;
     }
 
     public Integer sub(Integer a, Integer b){
